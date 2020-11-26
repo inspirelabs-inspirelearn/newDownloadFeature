1fc0294d9908e9d514f1e7a4fc23f8dce8dece5f
inspirelabs-inspirelearn <73635821+inspirelabs-inspirelearn@users.noreply.github.com>
2020-11-26 15:32:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,39 @@
+// var mediaContent = '{'+
+//   '"contentObject" : ['+
+//     '{"mediaFile":"mediaContent/videoOne.mp4"},'+
+//     '{"mediaFile":"mediaContent/audioTwo.mp3"}'+
+//   ']'+
+// '}';
+
+// var obj = JSON.parse(mediaContent);
+
+// function loadTheContent() {
+   
+//     // and this one is for looping
+//     var thevalue = 0;
+//     var readContent = "";
+//     for (i = 0; i < 2; i++) {
+//        document.getElementById("demo").innerHTML = "download in progress";
+// //         console.log(obj.contentObject[i].mediaFile);
+//         readContent = obj.contentObject[i].mediaFile;
+        
+// //      starting the download process here
+//         var link = document.createElement("a");
+//         link.download = obj.contentObject[i].mediaFile;
+//         link.href= obj.contentObject[i].mediaFile;
+//         link.click();
+//         link.remove();
+      
+//         //then here we will update the progress bar, updating the value is the idea here
+//         thevalue  = thevalue + 50; 
+//         document.getElementById("theProgress").style = "width:"+thevalue+"%;";
+//         document.getElementById("theValue").innerText = thevalue;
+//     }
+//      document.getElementById("demo").innerHTML = "download complete";
+
+
+//new code here, no loop
+
 var mediaContent = '{'+
   '"contentObject" : ['+
     '{"mediaFile":"mediaContent/videoOne.mp4"},'+
@@ -8,28 +44,9 @@ var mediaContent = '{'+
 var obj = JSON.parse(mediaContent);
 
 function loadTheContent() {
-   
-    // and this one is for looping
-    var thevalue = 0;
-    var readContent = "";
-    for (i = 0; i < 2; i++) {
-       document.getElementById("demo").innerHTML = "download in progress";
-//         console.log(obj.contentObject[i].mediaFile);
-        readContent = obj.contentObject[i].mediaFile;
-        
-//      starting the download process here
-        var link = document.createElement("a");
-        link.download = obj.contentObject[i].mediaFile;
-        link.href= obj.contentObject[i].mediaFile;
+    var link = document.createElement("a");
+        link.download = obj.contentObject[0].mediaFile;
+        link.href= obj.contentObject[0].mediaFile;
         link.click();
-        link.remove();
-      
-        //then here we will update the progress bar, updating the value is the idea here
-        thevalue  = thevalue + 50; 
-        document.getElementById("theProgress").style = "width:"+thevalue+"%;";
-        document.getElementById("theValue").innerText = thevalue;
-    }
-     document.getElementById("demo").innerHTML = "download complete";
-
-
-
+        link.remove();  
+}
