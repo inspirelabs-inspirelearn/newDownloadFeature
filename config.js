ccd94aaf7f7d062f17343ff08be9275e3cc75cd5
inspirelabs-inspirelearn <73635821+inspirelabs-inspirelearn@users.noreply.github.com>
2020-11-27 12:52:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,49 +1,58 @@
-// var mediaContent = '{'+
-//   '"contentObject" : ['+
-//     '{"mediaFile":"mediaContent/videoOne.mp4"},'+
-//     '{"mediaFile":"mediaContent/audioTwo.mp3"}'+
-//   ']'+
-// '}';
+var mediaContent = '{'+
+  '"contentObject" : ['+
+    '{"mediaFile":"mediaContent/audioTwo.mp3"},'+
+    '{"mediaFile":"mediaContent/videoOne.mp4"}'+
+    
+  ']'+
+'}';
 
-// var obj = JSON.parse(mediaContent);
-
-// function loadTheContent() {
-   
-//     // and this one is for looping
-//     var thevalue = 0;
-//     var readContent = "";
-//     for (i = 0; i < 2; i++) {
-//        document.getElementById("demo").innerHTML = "download in progress";
-// //         console.log(obj.contentObject[i].mediaFile);
-//         readContent = obj.contentObject[i].mediaFile;
-        
-// //      starting the download process here
-//         var link = document.createElement("a");
-//         link.download = obj.contentObject[i].mediaFile;
-//         link.href= obj.contentObject[i].mediaFile;
-//         link.click();
-//         link.remove();
-      
-//         //then here we will update the progress bar, updating the value is the idea here
-//         thevalue  = thevalue + 50; 
-//         document.getElementById("theProgress").style = "width:"+thevalue+"%;";
-//         document.getElementById("theValue").innerText = thevalue;
-//     }
-//      document.getElementById("demo").innerHTML = "download complete";
+var obj = JSON.parse(mediaContent);
 
 function loadTheContent() {
-   var a = document.createElement('a'); 
-   a.href="mediaContent/audioTwo.mp3";
-   a.download = "mediaContent/audioTwo.mp3";
-   a.click();
+    //this line is for accessing a single object
+    // document.getElementById("demo").innerHTML =
+    // obj.contentObject[1].mediaFile;
+    // console.log(obj.contentObject[1].mediaFile);
+
+    // and this one is for looping
+    var thevalue = 0;
+    var readContent = "";
+    for (i = 1; i < 2; i++) {
+       document.getElementById("demo").innerHTML = "download in progress";
+        console.log(obj.contentObject[i].mediaFile);
+        readContent = obj.contentObject[i].mediaFile;
+
+        // setting a standby timer
+        // setTimeout(procedure, 1000);
+
+        thevalue  = thevalue + 50; 
+        document.getElementById("theProgress").style = "width:"+thevalue+"%;";
+        // document.getElementById("demo").innerHTML = "download in progress";
+        //this is what we are going to apply using our method
+
+        var link = document.createElement("a");
+        link.download = obj.contentObject[i].mediaFile;
+        link.href= obj.contentObject[i].mediaFile;
+        link.click();
+        //then here we will update the progress bar, updating the value is the idea here
+    }
+    document.getElementById("demo").innerHTML = "download complete";
 }
- function initiateDownloads() {
-//                     var creatLink = document.getElementById("theDownloadLink");
-//                     creatLink.href="mediaContent/audioTwo.mp3";
-//                     creatLink.download = "mediaContent/audioTwo.mp3";
-//                     creatLink.click();
-                       document.getElementById("para").innerHTML = "download is now in progress";
-                
- }
-// document.getElementById("theButton").addEventListener('click', initiateDownloads);
-document.getElementById("theButton").addEventListener("click", initiateDownloads);
+
+
+
+document.getElementById("btnToLink").addEventListener("click", displayDate);
+function displayDate() {
+  document.getElementById("para").innerHTML = Date();
+}
+
+
+
+function initiateDownloads() {
+  // var creatLink = document.getElementById("theDownloadLink");
+  // creatLink.href="mediaContent/audioTwo.mp3";
+  // creatLink.download = "mediaContent/audioTwo.mp3";
+  // creatLink.click();
+
+  console.log("hello there!!!");
+}
